347d6cf9494cd8fef38e5231859cdba52c58dab0
bellamacha <145041818+bellamacha@users.noreply.github.com>
2025-01-15 16:40:56 -0600

    Create Wip.java
    I kinda forgot why I made this but it's on the robot

---
+++ mycopy/FtcRobotController-10.1.1/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Wip.java
@@ -0,0 +1,171 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+
+/* variables:
+claw (smart servo) - port - control hub
+intake (smart servo) - port - control hub
+wrist (core hex motor) - port 0 - expansion hub
+arm (60:1 hd hex motor) - port  1 - expansion hub
+wheel motors (12:1 hd hex motor) - ports 1-4 - control hub
+ */
+
+@TeleOp
+public class Wip extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    public DcMotor arm = null; //the 60:1 motor
+    public DcMotor wrist = null; //core hex motor
+    public CRServo intake = null; //the active intake servo (CR)
+    public Servo claw = null; // smart servo (not CR)
+
+    final double ARM_TICKS_PER_DEGREE =
+            28 // number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
+                    * 1/360.0; // we want ticks per degree, not per rotation
+
+    //variables for positions
+
+    //arm positions
+    final double ARM_COLLAPSED_INTO_ROBOT  = 0;
+    final double ARM_COLLECT               = 0 * ARM_TICKS_PER_DEGREE;
+    final double ARM_CLEAR_BARRIER         = 15 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SPECIMEN        = 90 * ARM_TICKS_PER_DEGREE;
+    final double ARM_SCORE_SAMPLE_IN_LOW   = 90 * ARM_TICKS_PER_DEGREE;
+    final double ARM_ATTACH_HANGING_HOOK   = 110 * ARM_TICKS_PER_DEGREE;
+    final double ARM_WINCH_ROBOT           = 10  * ARM_TICKS_PER_DEGREE;
+
+    /* Variables to store the speed the intake servo should be set at to intake, and deposit game elements. */
+    final double INTAKE_COLLECT    = -1.0;
+    final double INTAKE_OFF        =  0.0;
+    final double INTAKE_DEPOSIT    =  0.5;
+
+    /* Variables to store the positions that the wrist should be set to when folding in, or folding out. */
+    final double CLAW_FOLDED_IN   = 0.1667;
+    final double CLAW_FOLDED_OUT  = 0.5;
+
+    /* A number in degrees that the triggers can adjust the arm position by */
+    final double FUDGE_FACTOR = 15 * ARM_TICKS_PER_DEGREE;
+
+    /* Variables that are used to set the arm to a specific position */
+    double armPosition = (int)ARM_COLLAPSED_INTO_ROBOT;
+    double armPositionFudgeFactor;
+
+    final double LIFT_TICKS_PER_MM = (111132.0 / 289.0) / 120.0;
+
+    final double LIFT_COLLAPSED = 0 * LIFT_TICKS_PER_MM;
+    final double LIFT_SCORING_IN_LOW_BASKET = 0 * LIFT_TICKS_PER_MM;
+    final double LIFT_SCORING_IN_HIGH_BASKET = 480 * LIFT_TICKS_PER_MM;
+
+    double liftPosition = LIFT_COLLAPSED;
+
+    double cycletime = 0;
+    double looptime = 0;
+    double oldtime = 0;
+
+    double armLiftComp = 0;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
+        // arm declaration
+        arm = hardwareMap.get(DcMotor.class, "arm"); // 60:1 motor
+        // wrist declaration
+        wrist = hardwareMap.get(DcMotor.class, "wrist"); // core hex motor
+
+      
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        wrist.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        /* Define and initialize servos.*/
+        intake = hardwareMap.get(CRServo.class, "intake");
+        claw = hardwareMap.get(Servo.class, "claw");
+
+        /* Make sure that the intake is off, and the wrist is folded in. */
+        intake.setPower(INTAKE_OFF);
+        claw.setPosition(CLAW_FOLDED_OUT);
+
+
+
+        waitForStart();
+
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1) * 2;
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x + rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+
+            if(gamepad1.dpad_up){
+                arm.setPower(.75);
+                Thread.sleep(50);
+                arm.setPower(0);
+            }
+            else if(gamepad1.dpad_down){
+                arm.setPower(-0.75);
+                Thread.sleep(50);
+                arm.setPower(0);
+            }
+
+            if(gamepad1.triangle){
+                wrist.setPower(.75);
+                Thread.sleep(50);
+                wrist.setPower(0);
+            }
+            else if(gamepad1.cross){
+                wrist.setPower(-0.75);
+                Thread.sleep(50);
+                wrist.setPower(0);
+            }
+
+
+        }
+    }
+}
